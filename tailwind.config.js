82b1e2f5315ec31074626f46a174609a34a27564
LikeAaron <aaron.vanmarcke@student.howest.be>
2024-03-08 15:42:57 +0100

    initial push

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+  content: ['./index.html', './js/*.js'], // Specify the HTML and JavaScript files to search for classes
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
